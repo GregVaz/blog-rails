92f31e00bdb93040088d955073e3d17be1a20b48
Gregorio Vazquez <gregoriovazya@gmail.com>
2023-07-04 12:42:41 -0700

    Add helpers to blog posts model tests

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -2,35 +2,38 @@ require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
   test "draft? returns true for draft blog post" do
-    assert draft_blog_post.draft?
+    assert blog_posts(:draft).draft?
   end
 
   test "draft? returns false for publish blog post" do
-    refute published_blog_post.draft?
+    refute blog_posts(:published).draft?
   end
 
   test "draft? returns false for scheduled blog post" do
-    refute scheduled_blog_post.draft?
+    refute blog_posts(:scheduled).draft?
   end
   
+  test "published? returns false for draft blog post" do
+    refute blog_posts(:draft).published?
+  end
+
   test "published? returns true for published blog post" do
-    assert published_blog_post.published?
+    assert blog_posts(:published).published?
+  end
+
+  test "published? returns false for scheduled blog post" do
+    refute blog_posts(:scheduled).published?
   end
   
-  test "schedule? returns true for schedule blog post" do
-    assert scheduled_blog_post.scheduled?
+  test "scheduled? returns false for draft blog post" do
+    refute blog_posts(:draft).scheduled?
   end
 
-
-  def draft_blog_post
-    BlogPost.new(published_at: nil)
+  test "scheduled? returns false for published blog post" do
+    refute blog_posts(:published).scheduled?
   end
 
-  def published_blog_post
-    BlogPost.new(published_at: 1.month.ago)
-  end
-  
-  def scheduled_blog_post
-    BlogPost.new(published_at: 1.month.from_now)
+  test "scheduled? returns true for scheduled blog post" do
+    assert blog_posts(:scheduled).scheduled?
   end
 end
